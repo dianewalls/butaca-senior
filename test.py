7c3e39cd097bcb8bc87a3510407df609f177e68f
rpinoga_bci <rodrigo.pino@bci.cl>
2025-01-14 15:57:27 -0300

    session handling

--- test.py
+++ test.py
@@ -8,20 +8,6 @@ from flask_bcrypt import Bcrypt
 with app.app_context():
     db.create_all()
 
-    bcrypt = Bcrypt(app)
-
-    email = "inf.rpino@gmail.com"
-    password = 'hola'
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-    
-    #get all users
-    users = db.session.query(User).all()
-    
-    for user in users:
-        user.password = hashed_password
-        db.session.commit()
-        print(f"Password for user {user.email} updated successfully.")
-
     # Corrected query to fetch the user by email
     #user = db.session.query(User).filter_by(email=email).first()
     #if user:
